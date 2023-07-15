1dbd3619f78cb94fb9c90daa56100e5d4fa6f3bc
PROPERTY\214843 <mintumia8@gmail.com>
2023-07-15 12:48:39 +0600

    a little change

--- app/Http/Controllers/MintuHomeTest.php
+++ app/Http/Controllers/MintuHomeTest.php
@@ -3,25 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+
 class MintuHomeTest extends Controller
 {
 
     public function home(){
 
     //$role = Role::create(["name" => "writer"]);
-    //$permission = Permission::create(['name' => 'edit article']);
+    //$permission = Permission::create(['name' => 'update article']);
+
+/*$role = Role::findById(1);
+$permission = Permission::findById(1);
+$role->givePermissionTo($permission);*/
+
+     /*$permission = Permission::findById(3)->name;
+
+      auth()->user(2)->givePermissionTo($permission);*/
 
 
 
-       // echo "Hello World";
 
-
-   /*return     view(function (){
-
-            echo "Hello World";
-        });*/
     }
     //
 }
